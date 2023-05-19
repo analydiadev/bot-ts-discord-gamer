fe8e2cbcf684bcb24f6f519cf8e2994908d5c635
anamoreiradevs <analydiamoreira965@gmail.com>
2023-05-19 20:50:13 -0300

    ping-user

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -4,17 +4,8 @@ import { Discord, Slash } from "discordx";
 @Discord()
 class Ping {
     @Slash({ description: "pig de usuário" })
-    ping(interaction: CommandInteraction): void {
-       
-        // var data = new Date()
-        // console.log(data.toISOString);
-        // var format = data.toLocaleString()
-        // console.log(format);
-        // console.log(Date.parse(format));
-        
+    ping(interaction: CommandInteraction): void {  
         interaction.reply({ content: `Tenha uma excelente semana ${interaction.user.username}, seu ping é: (${Date.now() - interaction.createdTimestamp}ms)`, ephemeral: true }) 
-        interaction.client.user.setPresence({activities: [{name: "ESTOU TE VENDO!", type: ActivityType.Watching}], status: "online"})
-
     }
 }
 
